715eeca23f30e3111abfd81ce5910911a136eee1
Marckris <poramest.vic@ascendcorp.com>
2026-01-19 09:38:41 +0700

    feat: comment

--- question5kyu/moving_zero_to_end.go
+++ question5kyu/moving_zero_to_end.go
@@ -1,5 +1,8 @@
 package question5kyu
 
+//Write an algorithm that takes an array and moves all the zeros to the end, preserving the order of the other elements.
+//MoveZeros([]int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) // returns []int{ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }
+
 func MoveZeros(arr []int) []int {
 	newArr := []int{}
 	zeroCount := 0
